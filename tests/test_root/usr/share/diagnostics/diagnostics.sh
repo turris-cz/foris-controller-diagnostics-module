4bf714151ffcb582e35f82fe498125b9f05a2bc4
Karel Kočí <karel.koci@nic.cz>
2020-11-26 12:04:05 +0100

    tests: pull in turris diagnostics version 13.1

--- tests/test_root/usr/share/diagnostics/diagnostics.sh
+++ tests/test_root/usr/share/diagnostics/diagnostics.sh
@@ -1,107 +1,181 @@
 #!/bin/sh
 
+# set proper gettext domain
+export TEXTDOMAIN=turris-diagnostics
+export TEXTDOMAINDIR=/usr/share/locale
+
 MAX_LINES_PER_MODULE=${MAX_LINES_PER_MODULE:-100000}
+MODULES_PATH=./modules
 
 # enter the script directory
 cd "$(dirname $0)"
 
-# read modules and load help
-modules=""
-for mod_file in modules/*.module ; do
-	# load help
-	. "$mod_file"
-	module=$(basename "$mod_file" .module)
-	modules="$modules $module"
-
-done
-
-is_in_list() {
-	local list="$1"
-	local item="$2"
-	for element in $list ; do
-		if [ "$item" = "$element" ] ; then
-			return 0
-		fi
-	done
-	return 1
+module_exists() {
+	[ -x "$1" ]
 }
 
+module_name() {
+	local t="$(basename "${1%.module}")"
+	echo "${t#*_}" # remove initial index from XX_name
+}
+
+module_from_name() {
+	# There should be only one module of the same name but to be sure we
+	# potentially select the one with lower number.
+	find -name "??_$1.module" | head -1
+}
+
+
+list_modules() {
+	for module in "$MODULES_PATH"/*.module; do
+		[ -f "$module" ] || continue
+		echo "$(module_name "$module")"
+		"$module" help | sed 's/^/ /'
+	done
+}
+
+
 module_help() {
-	for module in $modules ; do
-		echo '  '${module}
-		./modules/"$module".module help | sed 's/^/    /'
+	for module in "$MODULES_PATH"/*.module; do
+		[ -f "$module" ] || continue
+		printf "  %s\n" "$(module_name "$module")"
+		"$module" help | sed 's/^/    /'
 		echo
 	done
 }
 
+print_usage() {
+	echo "Usage: $(basename $0) [-h | -l | -b] [-o <file> | -O <directory>] [module]..."
+}
+
 print_help() {
-	echo "$(basename $0) [-b] [-o <file> | -O <file>] [module1[ module2[...]]]"
-	echo "	-b		run in background"
-	echo "	-o <file>	print output to a file"
-	echo "	-O <file>	print output to a directory module per file"
+	print_usage
 	echo
-	echo modules:
+	echo "Arguments:"
+	echo "	-h		$(gettext "print this help text")"
+	echo "	-l		$(gettext "list all available modules in machine readable format")"
+	echo "	-b		$(gettext "run in background")"
+	echo "	-o <file>	$(gettext "print output to a file")"
+	echo "	-O <file>	$(gettext "print output to a directory module per file")"
+	echo
+	gettext "modules:"
+	echo
 	module_help
 }
+
 
 module_run() {
 	local module="$1"
 	if [ -n "$OUTPUT_DIRECTORY" ] ; then
-		./modules/"$module".module run 2>&1 | tail -n "$MAX_LINES_PER_MODULE" >> "$OUTPUT_DIRECTORY.preparing/$module".out.preparing
+		module_wrapper "$module" >> "$OUTPUT_DIRECTORY.preparing/$module.out.preparing"
 		mv "$OUTPUT_DIRECTORY.preparing/$module".out.preparing "$OUTPUT_DIRECTORY.preparing/$module".out
 	elif [ -n "$OUTPUT_FILE" ] ; then
-		printf "############## %s\n" $module >> "$OUTPUT_FILE".preparing
-		./modules/"$module".module run 2>&1 | tail -n "$MAX_LINES_PER_MODULE" >> "$OUTPUT_FILE".preparing
-		printf "************** %s\n" $module >> "$OUTPUT_FILE".preparing
+		{
+			module_header "$module"
+			module_wrapper "$module"
+			module_footer "$module"
+		} >> "$OUTPUT_FILE".preparing
 	else
-		printf "############## %s\n" $module
-		./modules/"$module".module run 2>&1 | tail -n "$MAX_LINES_PER_MODULE"
-		printf "************** %s\n" $module
+		module_header "$module"
+		module_wrapper "$module"
+		module_footer "$module"
 	fi
 }
 
-if [ "$1" = "-b" ] ; then
-	shift
-	"$0" "$@" >/dev/null 2>&1 &
+
+module_header() {
+	local module="$1"
+	printf "############## %s\n" "$(module_name "$module_name")"
+}
+
+
+module_footer() {
+	local module="$1"
+	printf "************** %s\n" "$(module_name "$module")"
+}
+
+
+module_wrapper() {
+	"$1" run 2>&1 < /dev/null | tail -n "$MAX_LINES_PER_MODULE"
+}
+
+
+OUTPUT_FILE=
+OUTPUT_DIRECTORY=
+list_modules="n"
+background="n"
+
+while getopts "hlbBo:O:" opt; do
+	case "$opt" in
+		h)
+			print_help
+			exit 0
+			;;
+		l)
+			list_modules="y"
+			;;
+		b)
+			background="y"
+			;;
+		o)
+			OUTPUT_FILE="$OPTARG"
+			;;
+		O)
+			OUTPUT_DIRECTORY="$OPTARG"
+			;;
+		*)
+			print_usage
+			exit 1
+	;;
+	esac
+done
+
+if [ "$background" = "y" -a "$TURRIS_DIAGNOSTICS_IN_BACKGROUND" != "1" ]; then
+	TURRIS_DIAGNOSTICS_IN_BACKGROUND=1 "$0" "$@" >/dev/null 2>&1 </dev/null &
 	exit 0
 fi
 
-if [ "$1" = "-o" ] ; then
-	shift
-	OUTPUT_FILE="$1"
-	# clean the last output files
+shift "$((OPTIND - 1))"
+
+if [ "$list_modules" = "y" ]; then
+	list_modules
+	exit 0
+fi
+
+
+# Clean the last output files
+if [ -n "$OUTPUT_FILE" ]; then
 	rm -rf "$OUTPUT_FILE"
 	rm -rf "$OUTPUT_FILE".preparing
-	shift
-fi
-
-if [ "$1" = "-O" ] ; then
-	shift
-	OUTPUT_DIRECTORY="$1"
+elif [ -n "$OUTPUT_DIRECTORY" ]; then
 	rm -rf "$OUTPUT_DIRECTORY" "$OUTPUT_DIRECTORY.preparing"
-	mkdir -p "$OUTPUT_DIRECTORY.preparing" || ( echo "Failed to created the log directory" && exit 1 )
-	shift
+	mkdir -p "$OUTPUT_DIRECTORY.preparing" || {
+		echo "Failed to created the log directory" >&2
+		exit 1
+	}
 fi
 
-if [ "$1" = help ] ; then
-	print_help
-	exit 0
-fi
 
-# no parameters run all modules
+module_executed="n"
 if [ $# = 0 ] ; then
-	modules_to_run=$modules
+	# no parameters run all modules
+	for module in "$MODULES_PATH"/*.module; do
+		[ -f "$module" ] || continue
+		module_run "$module"
+		module_executed="y"
+	done
 else
-	modules_to_run=$@
+	for module_name in "$@"; do
+		module="$(module_from_name "$module_name")"
+		if ! module_exists "$module"; then
+			printf "!!!!!!!!!!!!!! %s not found\n" "$module_name"
+		else
+			module_run "$module"
+			module_executed="y"
+		fi
+	done
 fi
 
-for module in $modules_to_run ; do
-	if ! is_in_list "${modules}" "${module}" ; then
-		printf "!!!!!!!!!!!!!! %s not found\n" $module
-	else
-		module_run "$module"
-	fi
-done
 
 # rename the output directory when finished
 if [ -n "$OUTPUT_DIRECTORY" ] ; then
@@ -112,3 +186,5 @@ else
 		mv "$OUTPUT_FILE".preparing "$OUTPUT_FILE"
 	fi
 fi
+
+[ "$module_executed" = "y" ]
--- tests/test_root/usr/share/diagnostics/modules/module.sh
+++ tests/test_root/usr/share/diagnostics/modules/module.sh
@@ -1,8 +1,47 @@
 #!/bin/sh
 
-echo_sec() {
+section() {
 	echo "== $@ =="
 }
+
+
+subsection() {
+	echo "-- $@ --"
+}
+
+
+file_header() {
+	local file="$1"
+	echo "~~ File: $1 ~~"
+}
+
+
+error() {
+	echo "!! $@ !!"
+}
+
+
+dump_file() {
+	local file="$1"
+	file_header "$file"
+
+	[ -f "$file" ] || {
+		error "File not found"
+		return 1
+	}
+
+	cat "$file"
+}
+
+
+check_installed() {
+	local program="$1"
+	which "$program" > /dev/null || {
+		error "Program '$program' not found"
+		return 1
+	}
+}
+
 
 . "$1"
 
@@ -17,5 +56,12 @@ case "$2" in
 		;;
 	run)
 		run
+		;;
+	*)
+		{
+			echo "Invalid usage of diagnostics module!"
+			echo "Usage: ${0##*/} (help|run)"
+		} >&2
+		exit 1
 		;;
 esac
